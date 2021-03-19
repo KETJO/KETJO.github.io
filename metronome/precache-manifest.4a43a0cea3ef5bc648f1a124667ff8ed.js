92f91a6a5155225fd7652593421f7638e324a822
KETJO <zharkeev.post@gmail.com>
2021-03-19 14:53:34 +0500

    adding metronomeTest

---
+++ metronome/precache-manifest.4a43a0cea3ef5bc648f1a124667ff8ed.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1f6124d08dc919b15a8d45a4941c19",
+    "url": "/metronome/android-chrome-192x192.png"
+  },
+  {
+    "revision": "5c811f5a2b3367cb7bce9ff0d1576bca",
+    "url": "/metronome/android-chrome-512x512.png"
+  },
+  {
+    "revision": "bd6f21feb50b443c7e66c86582f6206a",
+    "url": "/metronome/android-launchericon-144-144.png"
+  },
+  {
+    "revision": "730454fb806112621e0a526f425296bb",
+    "url": "/metronome/android-launchericon-96-96.png"
+  },
+  {
+    "revision": "d3c4c62e1da5f3198305c862747bf79a",
+    "url": "/metronome/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/metronome/browserconfig.xml"
+  },
+  {
+    "revision": "28a2f60823fffabbc210",
+    "url": "/metronome/css/app.08d5dc09.css"
+  },
+  {
+    "revision": "61292a821188deb000c515d2a41353b9",
+    "url": "/metronome/img/volume.61292a82.png"
+  },
+  {
+    "revision": "f99bf16520d40cba5ef016c59f253fbf",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "28a2f60823fffabbc210",
+    "url": "/metronome/js/app.b8bfa765.js"
+  },
+  {
+    "revision": "aa57879e81ec1ae1b1b2",
+    "url": "/metronome/js/chunk-vendors.b82c2a6c.js"
+  },
+  {
+    "revision": "87b6ca40e1382e886411ab77ba2fccc6",
+    "url": "/metronome/manifest.json"
+  },
+  {
+    "revision": "efa417a3da16fb8d8a68d9f253755a94",
+    "url": "/metronome/media/sounds/cahon/0.mp3"
+  },
+  {
+    "revision": "23907c1927ffffb9d07dd7b80da2f0d1",
+    "url": "/metronome/media/sounds/cahon/1.mp3"
+  },
+  {
+    "revision": "9ef280b7ef542640b14cdbfe46f799ab",
+    "url": "/metronome/media/sounds/cowbell/0.mp3"
+  },
+  {
+    "revision": "f5e09de16fe21cdbf0642afbec893b71",
+    "url": "/metronome/media/sounds/cowbell/1.mp3"
+  },
+  {
+    "revision": "3d0c930376516018157f08486a9f8969",
+    "url": "/metronome/media/sounds/hi-hat/0.mp3"
+  },
+  {
+    "revision": "e789db52989b2906ce2316d2e4d2f662",
+    "url": "/metronome/media/sounds/hi-hat/1.mp3"
+  },
+  {
+    "revision": "5fcf36795c3773b89dc5722c42414c91",
+    "url": "/metronome/media/sounds/rim-shot/0.mp3"
+  },
+  {
+    "revision": "efd1ebe0126bb4f7b6c2dc085ac4b492",
+    "url": "/metronome/media/sounds/rim-shot/1.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/metronome/robots.txt"
+  }
+]);
